da89ce46bc143c81fdf7cbbc738b85caa61fd95b
jptlightx005 <jptlightx005@gmail.com>
2017-03-08 16:19:13 +0800

    Change db connection

--- api/db.php
+++ api/db.php
@@ -1,6 +1,6 @@
 <?php
     date_default_timezone_set("Asia/Taipei");
-    $conn = new mysqli("localhost", "root", "", "osss_db");    
+    $conn = new mysqli("us-cdbr-iron-east-03.cleardb.net", "b9e27527065c19", "3540d53c", "heroku_91a85da334dee22");    
 
 	if(mysqli_connect_errno()){
 		printf("Connection failed: %s", mysqli_connect_error());
