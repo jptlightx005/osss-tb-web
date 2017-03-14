dfc0025326965f54c3c0a8066389c5aecef3c4b8
jptlightx005 <jptlightx005@gmail.com>
2017-03-14 13:24:10 +0800

    try another one

--- index.php
+++ index.php
@@ -1,20 +1,32 @@
 <?php
-require __DIR__ . "/vendor/autoload.php";
-use Mailgun\Mailgun;
+function send_mail($email,$subject,$msg) {
+	$api_key=getenv("MAILGUN_API_KEY");
+	$domain = getenv("MAILGUN_DOMAIN");
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+	curl_setopt($ch, CURLOPT_USERPWD, 'api:'.$api_key);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+	curl_setopt($ch, CURLOPT_URL, 'https://api.mailgun.net/v2/'.$domain.'/messages');
+	curl_setopt($ch, CURLOPT_POSTFIELDS, array(
+	'from' => "no-reply <no-reply@$domain.com>",
+	'to' => $email,
+	'subject' => $subject,
+	'html' => $msg
+	));
+	$result = curl_exec($ch);
+	curl_close($ch);
+	return $result;
+}
 
 if(isset($_POST["action"])){
 	echo "testing: {$_POST["action"]}<br>";
 	# Instantiate the client.
-	$mgClient = new Mailgun(getenv("MAILGUN_API_KEY"));
-	$domain = getenv("MAILGUN_DOMAIN");
+	$mgClient = new Mailgun();
+	
 
 	# Make the call to the client.
-	$result = $mgClient->sendMessage($domain, array(
-	    'from'    => getenv("MAILGUN_SMTP_LOGIN"),
-	    'to'      => $_POST["email"],
-	    'subject' => 'Hello',
-	    'text'    => 'Testing some Mailgun awesomness!'
-	));
+	$result = send_mail($_POST["email"], 'Hello', 'Testing some Mailgun awesomness!');
 	
 	echo "testing:<br>";
 	if($result){
